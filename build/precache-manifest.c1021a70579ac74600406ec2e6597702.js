6552a56048e583fe7940e7a7a4317eb466d3bd6c
Testers-png <61288366+Testers-png@users.noreply.github.com>
2020-02-20 20:51:45 +0300

    Add files via upload

---
+++ build/precache-manifest.c1021a70579ac74600406ec2e6597702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5310d411981f289db3ee755b94031e6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "908dca8a34717ccf209b",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "5a7379a79c4b7627ccbc",
+    "url": "./static/css/main.04b2ca86.chunk.css"
+  },
+  {
+    "revision": "908dca8a34717ccf209b",
+    "url": "./static/js/2.194bf7f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.194bf7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7379a79c4b7627ccbc",
+    "url": "./static/js/main.5b008001.chunk.js"
+  },
+  {
+    "revision": "b176e7b85fef74eda2c0",
+    "url": "./static/js/runtime-main.6eef6f70.js"
+  }
+]);
